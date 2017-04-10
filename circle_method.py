a03b57229c12f9a052c163ab657ca1662ff63959
Megan J Shearer <shearerj@caen-vnc-vm17.engin.umich.edu>
2017-04-10 15:12:27 -0400

    brackets

--- circle_method.py
+++ circle_method.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from generate_data import Dataset
 from defense import mesh
 from defense2 import BetterThanFrank
-#from qclp_cplex import QCLP
+from qclp_cplex import QCLP
 
 class Anomaly_Detector:
     
@@ -150,7 +150,7 @@ def greedy_optimal_attack(data, target, r):
     center = np.mean(data, axis=0)
     direction = target - center
     new_point = QCLP(data, direction, r)
-    return new_point
+    return [new_point]
 
 def main():
 
