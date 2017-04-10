6d11488d0d1ddcc6c1660a4ecf6f466de85c2f5b
emhou <emhou@umich.edu>
2017-04-10 14:20:30 -0400

    dfsdfsfs

--- defense2.py
+++ defense2.py
@@ -21,6 +21,17 @@ class BetterThanFrank:
             idx_suspect_pts = self.indices(avg_kNN_dist, lambda x: x > threshold)
         return idx_suspect_pts
 
+    def iGEM(self, NN=10, num_suspect=1, threshold=None):
+        dist_mat = euclidean_distances(self.buffer, self.buffer)
+        sorted_dist_mat = dist_mat
+        sorted_dist_mat.sort(axis=1)
+        avg_kNN_dist = np.mean(sorted_dist_mat[:, range(1, NN+1)], axis=1) # first neighbor is itself
+        if threshold == None:
+            idx_suspect_pts = avg_kNN_dist.argsort()[0:num_suspect]  # last points with highest distances
+        else:
+            idx_suspect_pts = self.indices(avg_kNN_dist, lambda x: x < threshold)
+        return idx_suspect_pts
+
 def main():
     data = Dataset(p=2, n=3000, phi=0.05)
     data.generate_data(standard=True, df=10)
@@ -28,7 +39,7 @@ def main():
     normal_data = data.generate_new_points(75)[1] # 75% of new points are normal
     suspect_data = np.random.uniform(2, 6, 50).reshape((25, 2)) # last 25 points are suspect
     buff_data = np.append(normal_data, suspect_data, axis=0)
-    s_pts = BetterThanFrank(buff_data).GEM(NN=30, num_suspect=25)
+    s_pts = BetterThanFrank(buff_data).iGEM(NN=30, num_suspect=25)
     
     plt.figure()
     plt.scatter(buff_data[-25:, 0], buff_data[-25:, 1], color='r', s=3)
